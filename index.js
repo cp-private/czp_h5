68c6be18afd1f067efae5d52dfc4d3181bb7334a
root <atwoodcai@163.com>
2018-05-14 20:35:04 +0800

    x

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = new Koa();
 global.middleware_path = `${__dirname}/app/middleware`;
 global.VIEW_PATH       = `${__dirname}/app/view`;
 global.APP_ROOT        = __dirname;
-global.mongo_url       = 'mongodb://120.92.107.209:8890';
+global.mongo_url       = 'mongodb://localhost:27017';
 global.BAIDU_API_KEY   = '9c052a5be4c17b6d9e87a23db4fad4c7';
 global.ALI_KEY         = process.env.ALI_KEY;
 
